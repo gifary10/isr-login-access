b36a4f5f5bef3f8b74c77e939e9cf30361f59d9d
gifary10 <57998309+gifary10@users.noreply.github.com>
2025-07-28 00:31:23 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'isr-cache-v2';
-const OFFLINE_URL = '/offline.html';
+const OFFLINE_URL = 'offline.html';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/splash.html',
-  '/offline.html',
-  '/css/isr.css',
-  '/js/cores.js',
-  '/js/handlers.js',
-  '/assets/main.css',
-  '/assets/main.js',
-  '/assets/nodeline.js',
-  '/logoh.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
+  
+  'index.html',
+  'splash.html',
+  'offline.html',
+  'css/isr.css',
+  'js/cores.js',
+  'js/handlers.js',
+  'assets/main.css',
+  'assets/main.js',
+  'assets/nodeline.js',
+  'logoh.png',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
