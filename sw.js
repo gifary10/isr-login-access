8e884eb9d4ba62da98c508866ad53fc4dbb7fb6c
gifary10 <57998309+gifary10@users.noreply.github.com>
2025-08-10 20:13:54 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,94 @@
+const CACHE_NAME = 'safety-report-v21';
+const ASSETS_TO_CACHE = [
+  '',
+  'index.html',
+  'css/style.css',
+  'js/app.js',
+  'js/modules/LoginSystem.js',
+  'js/modules/helpers/fetch.js',
+  'js/modules/helpers/offcanvas.js',
+  'js/modules/helpers/splash.js',
+  '2.png',
+  '3.png',
+  'logoh.png',
+  'logop.png',
+  'icons/icon-72x72.png',
+  'icons/icon-96x96.png',
+  'icons/icon-128x128.png',
+  'icons/icon-144x144.png',
+  'icons/icon-152x152.png',
+  'icons/icon-192x192.png',
+  'icons/icon-384x384.png',
+  'icons/icon-512x512.png',
+  'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+
+        // Otherwise fetch from network
+        return fetch(event.request)
+          .then((response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            // Cache the new response
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            // If fetch fails, return offline page or fallback
+            if (event.request.headers.get('accept').includes('text/html')) {
+              return caches.match('./offline.html');
+            }
+          });
+      })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
